8e217a67b29a9fd7743b2f64ebb00d146e0c770e
marco <marcomorenilla@gmail.com>
2026-01-24 14:01:49 +0100

    Avanzo con el proyecto integrando gemini

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import time
 import os
+import asyncio
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 from selenium import webdriver
@@ -8,15 +9,22 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from markdownify import markdownify as md
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-def get_html(url):
-    """
-    Usa un navegador real (Chrome) para cargar la página y ejecutar JS.
-    Devuelve el código fuente HTML completo.
-    """
+
+
+
+
+
+# Abre navegador y espera a que se ejecute el JS
+# Devuelve el código del HTML completo
+async def get_html(url):
+
     # Configuración de Chrome para que no se abra la ventana (Headless)
     chrome_options = Options()
-    # Si quieres ver el navegador abrirse, comenta la línea siguiente:
     chrome_options.add_argument("--headless") 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
@@ -27,13 +35,13 @@ def get_html(url):
     try:
         driver.get(url)
         
-        # Esperamos hasta 10 segundos a que aparezca al menos un enlace (tag 'a')
+        # Esperamos hasta 10 segundos hasta que aparezca un <a></a>
         try:
             WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.TAG_NAME, "a"))
             )
-        except:
-            print("  (Warning: Timeout esperando elementos, procesando lo que haya...)")
+        except Exception as e:
+            print(f"  Se produjo un error recuperando la web {url}: \n{e}")
 
         # Hacemos un pequeño scroll hacia abajo para activar "lazy loading" si lo hubiera
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
@@ -48,23 +56,105 @@ def get_html(url):
     finally:
         driver.quit() 
 
+async def ai_analyzer(model, system_instruction, contents):
+    client = genai.Client()
+
+    # Create a cache with a 5 minute TTL (300 seconds)
+    """
+    cache = client.caches.create(
+        model=model,
+        config=types.CreateCachedContentConfig(
+            display_name='recruiter', 
+            system_instruction=system_instruction,
+            contents=[contents],
+            ttl="300s",
+        )
+    )"""
+
+    response = client.models.generate_content(
+        model = model,
+        config=types.GenerateContentConfig(
+            system_instruction=system_instruction
+        ),
+        contents= [contents, 'Extrae nombre de la oferta, enlace si se ajustan al perfil descrito en las instrucciones']
+        #config=types.GenerateContentConfig(cached_content=cache.name)
+    )
+    
 
 
-def main():
+    return response
+
+# Conviertte el html a md para limpieza excluyendo etiquetas head, script, style
+def parse_html_to_md(html):
+    print('Cleaning and converting content...')
+    
+    # Usamos BeautifulSoup para una limpieza profunda
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    # Añadimos 'style', 'noscript', 'meta' y 'svg' para ahorrar más tokens
+    for element in soup(["script", "style", "head", "header", "footer", "nav", "noscript", "svg", "meta"]):
+        element.decompose()
+
+    # Seleccionamos solo el contenido principal si la web usa etiquetas semánticas
+    main_content = soup.find('main') or soup.find('article') or soup.body
+    
+    # Convertimos a Markdown lo que queda (que ya está limpio)
+    if main_content:
+        clean_html = str(main_content)
+        md_text = md(clean_html) 
+        
+        # Limpieza de saltos de línea excesivos
+        md_text = "\n".join([line.strip() for line in md_text.splitlines() if line.strip()])
+
+        with open('web.md', 'w', encoding='utf-8') as f:
+            f.write(md_text)
+        print('File web.md saved successfully.')
+
+
+
+
+async def main():
     # Nombre del fichero con las Url
     urls_file = 'urls.json'
-
+    system_instructions_file = 'system_instructions.md'
+    web_md_file = 'webs.md'
+    model = "gemini-2.5-flash"
+    print(f'Gemini API_KEY: {GEMINI_API_KEY[0:10]}....')
     try:
         with open(urls_file, 'r', encoding='utf-8') as f:
             urls = json.load(f).get('urls', [])
             print(urls)
+        with open(system_instructions_file, 'r', encoding='utf-8') as f:
+            instructions = []
+            for line in f:
+                instructions.append(line)
+            system_instructions = "\n".join(instructions)
+
+        with open(web_md_file, 'r', encoding='utf-8') as f:
+            web_content = []
+            for line in f:
+                web_content.append(line)
+            content = "\n".join(web_content)
+    
+
     except Exception:
         return
 
     # Palabras claves provisionales
     keywords = ["Junior", "python", "developer", "programador"]
-    all_results = []
+    html = await get_html(urls[0] if len(urls)>0 else '<h1>No content found</h1>')
+    parse_html_to_md(html)
+    gemini_response = await ai_analyzer(model, system_instructions, content)
+    print(gemini_response.text)
+
+
 
 
 if __name__ == "__main__":
-    main()
+    # cargamos variables de entorno
+    load_dotenv()
+
+    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+    
+    asyncio.run(main())
---
+++ webs.md
@@ -0,0 +1,132 @@
+Busca ofertas en Accenture
+==========================
+There are currently no jobs available in {0}. Please select a different country and continue your job search.
+SEARCH
+Featured Skills Search:
+[Primer empleo](?&jk=%22%C2%BFeres%20reci%C3%A9n%20titulado%3F%22)
+[Prácticas](?&jk=%22Dixcover%22)
+[SAP](?&jk=%22SAP%22)
+[Cloud](?&jk=%22CLOUD%22)
+[Salesforce](?&jk=%22SALESFORCE%22)
+When autocomplete options are available, use up
+and down arrows to review and enter to select.
+* TODOS LOS EMPLEOS (11)
+Ordenar por:
+[Más reciente](#)
+|
+[Lo más relevante](#)
+[NUEVA BÚSQUEDA DE OFERTAS](/es-es/careers/jobsearch)
+List View
+TILE VIEW
+FILTER RESULTS
+Filtrar Resultados
+------------------
+Update
+BORRAR TODO
+City
+[---
+España
+---
+Madrid
+Guardar
+### Ingeniero de Protecciones Junior
+Infrastructure & Capital Projects
+Hace más de un mes](https://www.accenture.com/es-es/careers/jobdetails?id=13686870_es&title=Ingeniero%20de%20Protecciones%20Junior)
+[---
+---
+Multiple Locations
+Guardar
+### Ingeniero Junior Big Data - AWS & Python
+AI & Data
+publicada 9 días publicada](https://www.accenture.com/es-es/careers/jobdetails?id=R00307228_es&title=Ingeniero%20Junior%20Big%20Data%20-%20AWS%20%26%20Python)
+[---
+---
+Multiple Locations
+Guardar
+### Technnical Consultant SAP PI, PO, CPI for Spain
+Business & Technology Integration
+Hace más de un mes](https://www.accenture.com/es-es/careers/jobdetails?id=R00235340_es&title=Technnical%20Consultant%20SAP%20PI%2C%20PO%2C%20CPI%20for%20Spain)
+[---
+---
+Multiple Locations
+Guardar
+### AI Supply Chain Strategy Manager
+AI & Data
+publicada 4 días publicada](https://www.accenture.com/es-es/careers/jobdetails?id=R00307440_es&title=AI%20Supply%20Chain%20Strategy%20Manager)
+[---
+---
+Multiple Locations
+Guardar
+### Supply Chain Data&AI Strategy Consultant
+AI & Data
+Hace más de un mes](https://www.accenture.com/es-es/careers/jobdetails?id=R00288934_es&title=Supply%20Chain%20Data%26AI%20Strategy%20Consultant)
+[---
+---
+Multiple Locations
+Guardar
+### Consultor SAP Basis
+Industry Solutions & Services
+publicada 4 días publicada](https://www.accenture.com/es-es/careers/jobdetails?id=R00235345_es&title=Consultor%20SAP%20Basis)
+[---
+---
+Multiple Locations
+Guardar
+### Microservices Software Engineering
+Software Engineering
+publicada 16 días publicada](https://www.accenture.com/es-es/careers/jobdetails?id=R00283710_es&title=Microservices%20Software%20Engineering)
+[---
+---
+Multiple Locations
+Guardar
+### Banking Strategy
+Consulting
+Hace más de un mes](https://www.accenture.com/es-es/careers/jobdetails?id=R00184766_es&title=Banking%20Strategy)
+[---
+España
+---
+Madrid
+Guardar
+### Workplace Services Analyst
+Workplace Management & Service Solutions
+publicada 10 días publicada](https://www.accenture.com/es-es/careers/jobdetails?id=R00285480_es&title=Workplace%20Services%20Analyst)
+[---
+---
+Multiple Locations
+Guardar
+### Senior SAP Integration Expert
+Business & Technology Integration
+publicada 16 días publicada](https://www.accenture.com/es-es/careers/jobdetails?id=R00305879_es&title=Senior%20SAP%20Integration%20Expert)
+[---
+---
+Multiple Locations
+Guardar
+### SAP BTP Development Lead – AI Expert
+Business & Technology Integration
+publicada 11 días publicada](https://www.accenture.com/es-es/careers/jobdetails?id=R00305911_es&title=SAP%20BTP%20Development%20Lead%20%E2%80%93%20AI%20Expert)
+No results for
+--------------
+###### Search tips
+* Please enter a different search term, or [click here](/es-es/careers/jobsearch) to begin a new job search
+* There are currently no jobs available that match your search criteria. Let us contact you when a matching job becomes available. [Sign up](https://privacyportal-de.onetrust.com/ui/#/preferences/multipage/login/74849542-5184-4a3a-8551-7d35999304aa?type=joinus) to receive job alerts.
+[---
+---
+Guardar
+###](#)
+[Anterior](/es-es/careers/)
+[1](#)
+Página de 1
+[Siguiente](/es-es/careers/)
+[Anterior](/es-es/careers/)
+[Siguiente](/es-es/careers/)
+### Igualdad de Oportunidades en el Empleo
+Todas las decisiones de empleo se tomarán sin importar la edad, raza, credo, color, religión, sexo, origen nacional, ascendencia, grado de discapacidad, estado de veterano, orientación sexual, identidad o expresión de género, información genética, estado civil, estado de ciudadanía o cualquier otro motivo o circunstancias protegidas por las leyes federales, estatales o locales.
+Mantente al día
+---------------
+[![](data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7)](/es-es/careers/jobsearch)
+### Únete al equipo
+Busca ofertas de empleo que coincidan con tus habilidades e intereses. Buscamos personas proactivas, curiosas, creativas con ganas de trabajar en equipo.
+[BUSCAR OFERTAS DE EMPLEO](/es-es/careers/jobsearch)
+[![](data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7)](https://www.accenture.com/us-en/blogs/blogs-careers)
+### Mantente al día
+Mantente al día de las últimas noticias, consejos y oportunidades profesionales.
+[LEER BLOG - ACCENTURE CAREERS](https://www.accenture.com/us-en/blogs/blogs-careers)
